0bb16036463f60ef1a2abdd6b6c809f41f03292b
ABDULBASITKHAN001 <[basitkhurramkhan@gmail.com]>
2025-02-03 13:07:24 +0500

    JS-ASGMT-CHAP-01

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("HELLO-WORLD")
+
+// CHAPTER NUMBER - 01 : ALERTS
+// QUESTION NUMBER 01: 
+ alert("WELCOME TO MY WEBSITE") 
+
+ //QUESTION NUMBER 02: 
+ alert("Error! Please enter a valid passworrd")
+    
+ // QUESTION NUMBER 03: 
+ alert("Welcome to JS Land... \n Happy Coding!");
+
+ //QUESTION NUMBER 04:
+ alert("Welcome to JS Land");
+alert("Happy Coding!");
+
+// QUESTION NUMBER 05:
+alert("Hello...I can run JS through my web browser's console");
+
+
+/////////////////////////// CHAPTER 01 COMPLETED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
